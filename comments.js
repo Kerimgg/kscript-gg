78fba2bb9e590183fa18e68e2a8130256fd6bee3
Master.1 <baklavapazari2@gmail.com>
2026-04-09 22:15:23 +0300

    Update comments.js

--- comments.js
+++ comments.js
@@ -2,13 +2,13 @@
 // Yeni yorumları buraya ekleyebilir, istemediklerini silebilirsin.
 const globalComments = [
     {
-        username: "Elite_Gamer",
-        comment: "The script hub is working perfectly on Xeno. Thanks!",
+        username: "Emuliu",
+        comment: "Thanks bro there are still people who provide such scripts for free without viruses",
         rating: 5
     },
     {
-        username: "DevMaster",
-        comment: "Great UI design, very modern and clean.",
+        username: "MasterGov",
+        comment: "Helal olsun amk mükemmel olmuş hiçbirşeyde sorun yaşamadım bide virüssüz uffff amk",
         rating: 5
     },
     // SİLMEK İSTEDİĞİN YORUMUN OLDUĞU BLOĞU ( { 'dan } 'a kadar ) SİLMEN YETERLİDİR.
