429feef042ad0d328dd248c70d5d82f6d6255178
Master.1 <baklavapazari2@gmail.com>
2026-04-09 22:09:08 +0300

    Create comments.js

---
+++ comments.js
@@ -0,0 +1,16 @@
+// KSCRIPT.GG YORUM VERİTABANI
+// Yeni yorumları buraya ekleyebilir, istemediklerini silebilirsin.
+const globalComments = [
+    {
+        username: "Elite_Gamer",
+        comment: "The script hub is working perfectly on Xeno. Thanks!",
+        rating: 5
+    },
+    {
+        username: "DevMaster",
+        comment: "Great UI design, very modern and clean.",
+        rating: 5
+    },
+    // SİLMEK İSTEDİĞİN YORUMUN OLDUĞU BLOĞU ( { 'dan } 'a kadar ) SİLMEN YETERLİDİR.
+    
+];
