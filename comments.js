39b98b55e62002cd06e75b16841978de1cc1c16d
Master.1 <baklavapazari2@gmail.com>
2026-04-09 23:10:20 +0300

    Update comments.js

--- comments.js
+++ comments.js
@@ -1,16 +1,4 @@
-// KSCRIPT.GG YORUM VERİTABANI
-// Yeni yorumları buraya ekleyebilir, istemediklerini silebilirsin.
 const globalComments = [
-    {
-        username: "Emuliu",
-        comment: "Thanks bro there are still people who provide such scripts for free without viruses",
-        rating: 5
-    },
-    {
-        username: "MasterGov",
-        comment: "Helal olsun amk mükemmel olmuş hiçbirşeyde sorun yaşamadım bide virüssüz uffff amk",
-        rating: 5
-    },
-    // SİLMEK İSTEDİĞİN YORUMUN OLDUĞU BLOĞU ( { 'dan } 'a kadar ) SİLMEN YETERLİDİR.
-    
+ { username: "Kullanici1", comment: "Harika site!", rating: 5 },
+ { username: "Kullanici2", comment: "Scriptler çalışıyor", rating: 4 }
 ];
